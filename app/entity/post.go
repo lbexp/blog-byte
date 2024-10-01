d1afdc34ec63113320f887a0d94ea4f01dd88699
lbexp <fiqarapr@gmail.com>
2024-10-02 06:31:33 +0800

    fix: adjust post entity

--- app/entity/post.go
+++ app/entity/post.go
@@ -3,10 +3,11 @@ package entity
 import "time"
 
 type Post struct {
-	Id        int
-	Title     string
-	Content   string
-	AuthorId  string
-	CreatedAt *time.Time
-	UpdatedAt *time.Time
+	Id         int
+	Title      string
+	Content    string
+	AuthorId   int
+	AuthorName string
+	CreatedAt  *time.Time
+	UpdatedAt  *time.Time
 }
